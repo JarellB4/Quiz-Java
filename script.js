cda0782d4d19088415aa8e4728b2206e6f050b2d
jarell Boone <jarellboone4@gmail.com>
2020-12-23 22:47:40 -0500

    redoing html and java

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
 const Submiit = document.querySelector("#start-btn");
-const questioionContainerElement = document. querySelector("question-container");
+const questioionContainerElement = document.querySelector("question-container");
 const Next = document.querySelector("#next-btn");
 const time = document.querySelector("#time");
-Submiit.addEventListener("click" , startQuiz);
+Submiit.addEventListener("click", startQuiz);
 var questions1 = document.getElementById("#question");
 var answers1 = document.getElementById("answer-btn");
 var secsLeft = 60;
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 
 
 
-var questions= [ {
 
- 
+let questions = [{
+
+
     name: "What is 1+1?",
-    answer:[
-        
-            {text:"2" , correct:true},
-            {text:"32", correct:false}
-         ]
+    answer: [
+
+        { text: "2", correct: true },
+        { text: "32", correct: false }
+    ]
 
 
 
 
 
-}, 
+},
 {
- 
- 
+
+
     name: "What is 2=2?",
-    answer:[
-        
-            {text:"4" , correct:true},
-            {text:"33", correct:false}
-         ]
+    answer: [
+
+        { text: "4", correct: true },
+        { text: "33", correct: false }
+    ]
 
 
 
 
-} ,
+},
 {
- 
- 
+
+
     name: "What is 3+1?",
-    answer:[
-        
-            {text:"5" , correct:true},
-            {text:"32", correct:false}
-         ]
+    answer: [
+
+        { text: "5", correct: true },
+        { text: "32", correct: false }
+    ]
 
 
 
 
-}, 
+},
 ]
 
 setTime();
@@ -59,47 +62,47 @@ setTime();
 
 function setTime() {
 
-var timerInterval = setInterval(function(){
-    secsLeft--;
-time.textContent=secsLeft;
-if(secsLeft===0){
-    clearInterval(timerInterval);
-}
-},1000);
+    var timerInterval = setInterval(function () {
+        secsLeft--;
+        time.textContent = secsLeft;
+        if (secsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
 }
 
 
 function startQuiz() {
-setTime();
-Submiit.classList.add("hide");
-questionContainerElement.classList.remove("hide");
-if (Submiit.style.display === "none") {
-    Submiit.style.display = "block";
-  } else {
-    Submiit.style.display = "none";
-setNextQuestion();
-}
+    setTime();
+    Submiit.classList.add("hide");
+    questionContainerElement.classList.remove("hide");
+    if (Submiit.style.display === "none") {
+        Submiit.style.display = "block";
+    } else {
+        Submiit.style.display = "none";
+        setNextQuestion();
+    }
 
 
 
 
 }
 function setNextQuestion() {
-questions1.innerText=questions.name
-questions.answer.forEach(answer => {
-    var btn=document.createElement("button");
-    btn.innerText=answer.text;
-    btn.classList.add("btn");
-if(answer.correct){
-    btn.dataset.correct=answer.correct
-}
-btn.addEventListener("click", selectAnswer);
-answerbtnelement
-});
+    questions1.innerText = questions.name
+    questions.answer.forEach(answer => {
+        var btn = document.createElement("button");
+        btn.innerText = answer.text;
+        btn.classList.add("btn");
+        if (answer.correct) {
+            btn.dataset.correct = answer.correct
+        }
+        btn.addEventListener("click", selectAnswer);
+        answerbtnelement
+    });
 }
 
 function showQuestions() {
-questiomElement.innerText = questions.questions
+    questiomElement.innerText = questions.questions
 
 }
 
